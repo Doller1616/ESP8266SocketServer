2149896e96004cb6c0d543d6c59b7ab627d9398a
Abhishek <aabhishekbbhardwaj@gmail.com>
2022-12-11 21:17:23 +0530

    add post api

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const POST = process.env.PORT || '8080';
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 
 app.get('/', (req, res) => {
     res.send("Hi! Divyansh Kumar")
 })
 
+app.post('/hi', (req, res)=> {
+  console.log(req.body, req.query, req.params);
+  res.send("Working");
+})
+
 app.listen(POST, () => {
-  console.log(`Server started on http://localhost:${POST}`)
+  console.log(`Server started on http://localhost:${POST}`);
 })
